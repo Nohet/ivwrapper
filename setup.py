1ce2ea2576549659b3f67c12d5854d570d5aa357
Nohet <70505658+Nohet@users.noreply.github.com>
2021-07-15 11:08:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="ivwrapper",
-    version="1.0",
+    version="1.1",
     description="Asynchroniczny wrapper dla api ivall'a.",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Nohet/ivwrapper",
     author="Nohet",
