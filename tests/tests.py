a7a5b25d9c50eeb1853f5f94a5b9cfc51dce08e6
Nohet <70505658+Nohet@users.noreply.github.com>
2021-07-16 09:08:23 +0200

    Add files via upload

---
+++ tests/tests.py
@@ -0,0 +1,16 @@
+import asyncio
+
+import ivwrapper
+
+iv = ivwrapper.Api()
+
+
+async def main():
+    joke = await iv.get_joke()
+    print(joke)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
