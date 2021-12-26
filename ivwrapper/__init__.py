c0f42f25450af9f7ac5f990f687585266e440c3f
Nohet <70505658+Nohet@users.noreply.github.com>
2021-12-26 19:40:33 +0100

    Update __init__.py

--- ivwrapper/__init__.py
+++ ivwrapper/__init__.py
@@ -3,7 +3,7 @@ from .ivwrapper import *
 __title__ = "ivwrapper"
 __summary__ = "Asynchroniczny wrapper dla api ivall'a."
 __uri__ = "https://github.com/Nohet/ivwrapper"
-__version__ = "1.3"
+__version__ = "1.4.1"
 __author__ = "Nohet"
 __email__ = "igorczupryniak503@gmail.com"
 __license__ = "MIT License"
